15accbe49375e7bcea28717b91de285fdd4b4085
Samantha Davis <sdavis@samanthsmacbook.home>
2020-10-25 19:59:22 -0400

    Add home screen headshot image and update styles

--- src/components/headshot.js
+++ src/components/headshot.js
@@ -1,6 +1,6 @@
-import React from "react"
-import { useStaticQuery, graphql } from "gatsby"
-import Img from "gatsby-image"
+import React from "react";
+import { useStaticQuery, graphql } from "gatsby";
+import Img from "gatsby-image";
 
 /*
  * This component is built using `gatsby-image` to automatically serve optimized
@@ -13,24 +13,30 @@ import Img from "gatsby-image"
  * - `useStaticQuery`: https://www.gatsbyjs.com/docs/use-static-query/
  */
 
-const Image = () => {
+const Headshot = () => {
   const data = useStaticQuery(graphql`
     query {
-      placeholderImage: file(relativePath: { eq: "gatsby-astronaut.png" }) {
+      file(relativePath: { eq: "headshot.jpeg" }) {
         childImageSharp {
-          fluid(maxWidth: 300) {
+          fluid(maxWidth: 700) {
             ...GatsbyImageSharpFluid
           }
         }
       }
     }
-  `)
+  `);
 
-  if (!data?.placeholderImage?.childImageSharp?.fluid) {
-    return <div>Picture not found</div>
+  if (!data?.file?.childImageSharp?.fluid) {
+    return <div>Picture not found</div>;
   }
 
-  return <Img fluid={data.placeholderImage.childImageSharp.fluid} />
-}
+  return (
+    <Img
+      alt="Samantha Davis head shot"
+      fadeIn
+      fluid={data.file.childImageSharp.fluid}
+    />
+  );
+};
 
-export default Image
+export default Headshot;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,28 +1,41 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from "react";
+import { Link } from "gatsby";
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import Layout from "../components/layout";
+import Headshot from "../components/headshot";
+import SEO from "../components/seo";
+import "./index.css";
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <p>Hi, I'm Sam.</p>
-    <p>
-      I'm a <span style={{fontSize: 40}}>full stack software engineer</span>, but I didn't start out that way.
-    </p>
-    <p>I'm passionate about learning and getting people the information they need. That's why I decided 
-      to attend a three month bootcamp and change my career from marketing and SEO to software development.
-    </p>
-    <p>Since then my experiences have ranged from building greenfield projects to 
-      supporting large applications serving global audiences. No matter what, my approach has always 
-      been based on curiosity, learning, and clear communication.
-    </p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div className="container">
+      <div className="body introText">
+        <p>Hi, I'm Sam.</p>
+        <p>
+          I'm a full stack software engineer, but I didn't start out that way.
+        </p>
+      </div>
+      <div className="headshotContainer">
+        <div style={{ flex: 1 }}>
+          <Headshot />
+        </div>
+        <div className="body secondaryBody">
+          <p>
+            I'm passionate about learning and getting people the information
+            they need. That's why I decided to attend a three month bootcamp and
+            change my career from marketing and SEO to software development.
+          </p>
+          <p>
+            Since then my experiences have ranged from building greenfield
+            projects to supporting large applications serving global audiences.
+            No matter what, my approach has always been based on curiosity,
+            learning, and clear communication.
+          </p>
+        </div>
+      </div>
     </div>
   </Layout>
-)
+);
 
-export default IndexPage
+export default IndexPage;
